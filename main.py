b0e9b734287c30f4b8875c55b224e4758491710d
TejasMJ <tejas21files@gmail.com>
2026-08-05 23:27:13 +0530

    added the first function

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/items/")
+def create_item(item: str):
+    items.append(item)
+    return items
